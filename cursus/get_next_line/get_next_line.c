bb08bf5236d88cabd3cf2112cc3c21019514ff3d
LDW <leehan02@hanyang.ac.kr>
2023-01-14 12:05:58 +0900

    adding 4 functions which are find_node, add_node, read_file, ft_strjoin & clearing 1 function which is ft_strcat

--- cursus/get_next_line/get_next_line.c
+++ cursus/get_next_line/get_next_line.c
@@ -1,27 +1,38 @@
 #include "get_next_line.h"
 
+#include <stdio.h>
+
 char    *get_next_line(int fd)
 {
-    char            *buf;
+    static t_list   lst;
     char            *line;
-    int             read_size;
-    static t_node   node;;   
-
-    node.fd = fd;
-    buf = (char*)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-    if (!buf)
-        return (0);
+    char	        *buf;
+    
     if (fd == -1)
         return (0);
-    while ((read_size = read(fd, buf, BUFFER_SIZE)) > 0)
+    if (!find_node(fd, &lst)) // input으로 들어온 fd를 가지고 있는 node가 현재 연결리스트에 없으면 0 반환
     {
-        buf[read_size] = '\0';
-        if (!line)
-            line = ft_strdup(buf);
-        else
-            line = ft_strcat(line, buf);
-        if ((buf = ft_strchr(buf, '\n')) != 0)
-            node.backup = ft_strdup(buf + 1); // buf에서 개행이후 읽어온 부분 저장
+        if (!add_node(fd, &lst))
+            return (0);
     }
+    buf = (char*)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+    if (!buf)
+		return (0);
+        //return (remove_node(lst));
+        
+    line = read_file(&lst, buf);
     return (line);
 }
+
+/*
+int remove_node(t_list *lst)
+{
+    t_node  *temp;
+
+    temp = lst->cur;
+    lst->cur = lst->cur->next;
+    free(temp);
+
+    return (0);
+}
+*/
--- cursus/get_next_line/get_next_line.h
+++ cursus/get_next_line/get_next_line.h
@@ -4,10 +4,26 @@
 typedef struct s_node {
     int     fd;
     char    *backup;
-    t_node  *next;
-} t_node;
+    struct s_node  *next;
+}   t_node;
 
-# include <fcntl.h>
+typedef struct s_list {
+    t_node  *head;
+    t_node  *cur;
+    //t_node  *before;  
+    //t_node  *tail;
+}   t_list;
+
+# include <unistd.h>
 # include <stdlib.h>
+
+char    *get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(char *src);
+unsigned int	ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+int	    find_node(int fd, t_list *lst);
+int	    add_node(int fd, t_list *lst);
+char	*read_file(t_list *lst, char* buf);
 
 #endif
